02085c1ae5527b9ba11cd952b61f95e06d667c7a
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-18 17:04:28 +0900

    Refactored form and fixed styles

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -20,9 +21,31 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+      @task.completed = task_params[:completed] == '1'
+
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit.html.erb'
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
