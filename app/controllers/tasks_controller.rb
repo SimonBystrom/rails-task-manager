21d92342b591e6c71b613bf70ead42d338552d9c
Simon Bystrom <simon.bystrom@hyperisland.se>
2021-05-18 12:47:14 +0900

    form almost completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,23 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    raise
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+
+  get '/tasks/new', to: 'tasks#new', as: :task
+
+  get '/tasks', to: 'tasks#index', as: :tasks
+  get '/tasks/:id', to: 'tasks#show'
+
+  post '/tasks', to: 'tasks#create'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
